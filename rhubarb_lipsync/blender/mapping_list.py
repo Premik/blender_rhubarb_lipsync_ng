485fe1757c1438b04e9f7bd97732432dfa8f9d23
Premik <premysl.srubar@post.cz>
2023-12-23 08:48:01 +0100

    Cleanup

--- rhubarb_lipsync/blender/mapping_list.py
+++ rhubarb_lipsync/blender/mapping_list.py
@@ -91,60 +91,36 @@ def draw_mapping_item_multiline(ctx: Context, layout: UILayout, mp:MappingProper
     mlp: MappingPreferences = prefs.mapping_prefs
     clp: CueListPreferences = prefs.cue_list_prefs
     item = mp.items[itemIndex]
+    selected = bool(mp.index == itemIndex)
 
-    layout = layout.box().column(align=True)
-    split = layout.split(factor=0.1)
-    c1 = split.column()
-    c2 = split.column()
+    
+    split1 = layout.split(factor=0.1)
+    
+    col1 = split1.column()
+    col2 = split1.column()
 
-    c1.label(text="c1")
-    c1.label(text="c1")
-    c2.label(text="c2")
-    c1.label(text="c1")
-    c2.label(text="c2")
-    c2.label(text="c2")
-    c2.label(text="c2")
+    if clp.as_circle:
+        col1.label(text=item.cue_desc.key_displ)
+    else:
+        col1.label(text=item.key)
+    
+    
+    if selected:
+        col2.label(text="")
+        col1.label(text="", icon="OBJECT_DATAMODE")
+        col2.prop(item, 'action', text="")
+        col1.label(text="", icon="SHAPEKEY_DATA")
+        col2.prop(item, 'shapekey_action', text="")
+        col1.label(text="", icon="EVENT_TAB")
+    
+    #EVENT_TAB
+    #DRIVER_DISTANCE
+    #ACTION_TWEAK
+    #SEQ_STRIP_DUPLICATE
+    
 
     return
     
-    row = split.row()
-    #row = split
-    
-    
-    #row.template_icon(icon_value=IconsManager.cue_image(item.key), scale=5)
-    # row = split.row()
-
-    
-    if not prefs.use_extended_shapes and item.cue_desc.extended:
-        row.enabled = False  # Indicate extended shape not in use
-    
-    if clp.as_circle:
-        row.label(text=item.cue_desc.key_displ)
-    else:
-        row.label(text=item.key)
-
-
-    row = split.row()
-    if mp.nla_map_action:
-        row.prop(item, 'action', text="")
-        
-        #row.template_ID(item, "action")
-        #row.template_ID_preview(item, "action")
-        #row.template_any_ID(item, "action")
-        
-
-    
-    row = split.row()
-    row.label(text=item.key)
-    #if mp.nla_map_shapekey:
-    row.prop(item, 'shapekey_action', text="")
-    # row.template_ID(item, "action", new="action.new", unlink="action.unlink")
-
-    
-
-    if mlp.show_help_button:
-        row.operator(mapping_operators.ShowCueInfoHelp.bl_idname, icon="QUESTION", text="").key = item.key
-
 class MappingUIList(UIList):
     bl_idname = "RLPS_UL_mapping"
 
