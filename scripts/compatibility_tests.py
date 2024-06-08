74a764c5aa0c40ad1da54d4f9c6511f23a21f12b
Premik <premysl.srubar@post.cz>
2024-06-08 22:13:31 +0200

    Compatibility tests draft

---
+++ scripts/compatibility_tests.py
@@ -0,0 +1,105 @@
+from functools import cached_property
+import json
+import os
+import pathlib
+import platform
+import shutil
+from dataclasses import dataclass, field
+from pathlib import Path
+import subprocess
+import tempfile
+
+
+@dataclass
+class BlenderSetup:
+    root_path: Path = field(default_factory=lambda: Path(tempfile.mkdtemp(prefix="blenderFresh_")))
+    target_platform: str = field(default_factory=lambda: platform.system())
+    blender_ver: str = "4.2"
+    addon_src: Path = Path(pathlib.Path(__file__).parent.parent)
+
+    @cached_property
+    def user_dir(self) -> Path:
+        return self.root_path / "blender" / self.blender_ver
+
+    @cached_property
+    def addons_path(self) -> Path:
+        return self.user_dir / "scripts" / "addons"
+
+    @cached_property
+    def test_results_path(self) -> Path:
+        return self.addons_path / f"test_results.json"
+
+    def set_blender_user_data(self) -> None:
+        xdg_config_home = self.root_path
+        env_var = "BLENDER_USER_RESOURCES" if self.target_platform == "Windows" else "XDG_CONFIG_HOME"
+        os.environ[env_var] = str(xdg_config_home)
+        # os.environ['TEST_RESULTS_PATH'] = str(self.test_results_path)
+
+    def install_addon(self):
+        self.addons_path.mkdir(parents=True, exist_ok=True)
+        target = self.addons_path / "rhubarb_lipsync"
+        if not target.exists():
+            os.symlink(self.addon_src / "rhubarb_lipsync", target)
+
+    def install_tests(self):
+        test_files = list(self.addon_src.glob("tests/*.py"))
+        for test_file in test_files:
+            shutil.copy(test_file, self.addons_path)
+
+        data_src = self.addon_src / "tests" / "data"
+        data_dst = self.addons_path / "data"
+        if data_dst.exists():
+            shutil.rmtree(data_dst)
+        shutil.copytree(data_src, data_dst)
+
+        aud_file = self.addons_path / "aud.py"
+        if aud_file.exists():
+            aud_file.unlink()
+
+    def setup_config(self):
+        config_dir = self.user_dir / "config"
+        config_dir.mkdir(parents=True, exist_ok=True)
+
+    def run_blender(self):
+        script = self.addons_path / "run_within_blender.py"
+        # exe = 'blender-4.2'
+        exe = 'wine "/var/wine/64loc/drive_c/Program Files/Blender Foundation/Blender 3.5/blender.exe"'
+        script = subprocess.check_output(['winepath', '-w', str(script)]).decode().strip()
+        cmd = f'''{exe} --background --python "{script}"'''
+        print(cmd)
+        os.system(cmd)
+
+    def verify_result(self):
+        if not self.test_results_path.exists():
+            raise FileNotFoundError(f"Test results file not found at {self.test_results_path}")
+
+        with self.test_results_path.open('r') as file:
+            test_results = json.load(file)
+
+        total_tests = test_results.get("total_tests", 0)
+        total_failed = test_results.get("total_failed", 0)
+
+        if total_failed > 0:
+            raise AssertionError(f"There are {total_failed} failed tests.")
+
+        if total_tests < 44:
+            raise AssertionError(f"Total tests are {total_tests}, expected at least 44 tests, seems some were skip")
+
+        print("All checks passed.")
+
+    def install_and_run(self):
+        self.setup_config()
+        self.install_addon()
+        self.install_tests()
+        self.set_blender_user_data()
+        if self.test_results_path.exists():
+            self.test_results_path.unlink()
+        self.run_blender()
+        self.verify_result()
+
+
+if __name__ == "__main__":
+    # setup = BlenderSetup(root_path=Path("/tmp/work/fresh2"), )
+    setup = BlenderSetup(root_path=Path("/tmp/work/freshWin"), target_platform="Windows", blender_ver="3.5")
+    # setup = BlenderSetup(root_path=Path("/tmp/work/fresh"), target_platform="Linux", blender_ver="4.2")
+    setup.install_and_run()
---
+++ tests/run_within_blender.py
@@ -0,0 +1,104 @@
+import os
+from pathlib import Path
+import platform
+import unittest
+import sample_data
+import inspect
+import importlib
+import json
+import bpy
+import sys
+
+for p in sys.path:
+    print(p)
+
+for p in sys.path:
+    print(p, file=sys.stderr)
+
+# sample_project.SampleProject.blender_as_module = False
+
+
+# def pytest_configure(config) -> None:
+#     config.option.log_cli = True
+#     config.option.log_cli_level = "DEBUG"
+
+
+addons_path: Path = sample_data.sample_data_path.parent
+
+# For some reason default value is:
+# blender/4.2/scripts/       rhubarb_lipsync/bin/rhubarb`
+# blender/4.2/scripts/addons/rhubarb_lipsync/bin/
+#
+# print('!!!!!')
+# print(bpy.utils.user_resource('SCRIPTS', path="addons"))
+# prefs: Path = RhubarbAddonPreferences.from_context(bpy.context)
+# print(prefs.executable_path_string)
+# addons/rhubarb_lipsync/bin/
+
+
+# Discover all test modules in the addons_path
+def discover_test_modules(path: Path):
+    test_modules = []
+    for file in path.rglob('test_*.py'):
+        # Convert file path to module name
+        module_name = str(file.relative_to(path)).replace('/', '.').replace('\\', '.').replace('.py', '')
+        test_modules.append(module_name)
+    return test_modules
+
+
+test_modules = discover_test_modules(addons_path)
+
+# test_modules = [
+#     'test_ui_utils',
+#     'test_baking_preparation',
+#     'test_process_sound_file',
+# ]
+print(f"Discovered test modules: {test_modules}")
+
+
+def load_tests_from_module(module_name) -> unittest.TestSuite:
+    module = importlib.import_module(module_name)
+    suite = unittest.TestSuite()
+    for name, obj in inspect.getmembers(module):
+        if inspect.isclass(obj) and issubclass(obj, unittest.TestCase):
+            suite.addTests(unittest.TestLoader().loadTestsFromTestCase(obj))
+    return suite
+
+
+# Discover and load all tests from the specified modules
+all_tests = unittest.TestSuite()
+for module_name in test_modules:
+    all_tests.addTests(load_tests_from_module(module_name))
+
+print(f"Running tests {all_tests}")
+
+
+def create_test_report(result):
+    report = {
+        "blender_version": bpy.app.version_string,
+        "system": platform.system(),
+        "total_tests": result.testsRun,
+        "total_passed": result.testsRun - len(result.failures) - len(result.errors) - len(result.skipped),
+        "total_failed": len(result.failures),
+        "total_errors": len(result.errors),
+        "total_skipped": len(result.skipped),
+    }
+    return report
+
+
+runner = unittest.TextTestRunner(verbosity=2)
+result = runner.run(all_tests)
+print("Done")
+
+
+env_log_file_path = os.getenv('TEST_RESULTS_PATH')
+default_log_file_path = Path(addons_path / 'test_results.json')
+
+log_file_path = Path(env_log_file_path) if env_log_file_path else default_log_file_path
+
+with open(log_file_path, 'w') as log_file:
+    # Create and write JSON report
+    report = create_test_report(result)
+    json.dump(report, log_file, indent=4)
+
+print(f"Test summary is saved to {log_file_path}")
